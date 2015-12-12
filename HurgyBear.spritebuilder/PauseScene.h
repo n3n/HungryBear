2aa21e158d30bb201d8c925c64c18b1ebfe40b9c
Nonpawit Teerachetmongkol <fallengoplzz@gmail.com>
2015-12-12 11:15:02 +0700

    New Pause Scene

---
+++ HurgyBear.spritebuilder/PauseScene.h
@@ -0,0 +1,13 @@
+//
+//  PauseScene.h
+//  HurgyBear
+//
+//  Created by Nonpawit Teerachetmongkol on 12/12/2558 BE.
+//  Copyright © 2558 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface PauseScene : CCNode
+
+@end
---
+++ HurgyBear.spritebuilder/PauseScene.m
@@ -0,0 +1,17 @@
+//
+//  PauseScene.m
+//  HurgyBear
+//
+//  Created by Nonpawit Teerachetmongkol on 12/12/2558 BE.
+//  Copyright © 2558 Apportable. All rights reserved.
+//
+
+#import "PauseScene.h"
+
+@implementation PauseScene
+
+- (void)resumeButtonTapped {
+    CCLOG(@"Resume ButtonTapped");
+}
+
+@end
